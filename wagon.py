6be00d04e2838b46ddd70fcbeb85f762242de9c7
Seamus Smith <student.141504@worcesterschools.net>
2020-10-30 17:26:45 +0000

    Added taking from and adding to an itemstack

--- wagon.py
+++ wagon.py
@@ -1,4 +1,4 @@
-from typing import union
+from typing import Union
 
 class Wagon:
   def __init__(self, inventory_size: int):
@@ -6,11 +6,27 @@ class Wagon:
     self.__max_inventory_size: int = inventory_size
     self.oxen: list = []
     self.party: list = []
-  
+  def add_item(self, ) -> bool:
+    pass
+  def find_item() -> bool:
+    pass
+  def take_item() -> bool:
+    pass
   pass
 
 class ItemStack():
-  def __init__(self, itemtype: type, max_size: int):
-    self.max_size = max_size
-    self.itemtype = itemtype
-  
+  def __init__(self, itemtype: type, max_size: int, count: int = 0):
+    self.__max_size = max_size
+    self.__itemtype = itemtype
+    self.__item_count = count
+  def add_item(self, item) -> bool:
+    if self.__item_count >= self.__max_size:
+      return False
+    else:
+      self.__item_count += 1
+    return True
+  def take_from_stack(self) -> Union[type, None]:
+    if self.__item_count > 0:
+      return self.__itemtype
+    else:
+      return None
