015157008780a7e5d59a0a690492a75d6822ba74
Seamus Smith <student.141504@worcesterschools.net>
2020-10-30 17:34:20 +0000

    Started work on items system

--- wagon.py
+++ wagon.py
@@ -19,11 +19,11 @@ class ItemStack():
     self.__max_size = max_size
     self.__itemtype = itemtype
     self.__item_count = count
-  def add_item(self, item) -> bool:
-    if self.__item_count >= self.__max_size:
+  def add_items(self, count: int) -> bool:
+    if self.__item_count + count > self.__max_size:
       return False
     else:
-      self.__item_count += 1
+      self.__item_count += count
     return True
   def take_from_stack(self) -> Union[type, None]:
     if self.__item_count > 0:
