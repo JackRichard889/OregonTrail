1851d5d532de3816f4b0fe3cd73d1d8d6c9bbd75
Jack Richard <student.145024@worcesterschools.net>
2020-10-30 16:51:25 +0000

    Initial

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+WIDTH, HEIGHT = 500, 500
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Oregon Trail 2020 Remastered Ultimate Edition")
+clock = pygame.time.Clock()
+
+all_sprites = pygame.sprite.Group()
+
+def main():
+  running = True
+  while running:
+    for event in pygame.event.get():
+      if event == pygame.QUIT:
+        running = False
+
+if __name__ == "__main__":
+  main()
+
+# testing from VSCode
