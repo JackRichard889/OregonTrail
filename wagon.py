a64f37d0c0095d1b847bd3d09eb137a2b78a61ff
Seamus Smith <student.141504@worcesterschools.net>
2020-11-03 17:47:01 +0000

    added update_party method to Wagon

--- wagon.py
+++ wagon.py
@@ -1,13 +1,20 @@
 from typing import Union
 from itembaseclass import BaseItem
 from itemstack import ItemStack
+from date import Date
 
 class Wagon:
   def __init__(self, party: list):
     self.inventory: dict = {
       "food": 0,
       "cash": 0,
-      "clothing": 0
+      "clothing": 0,
+      "ammunition": 0,
+      "axel": 0,
+      "wheel": 0
     }
     self.oxen: list = []
     self.party: list = []
+  def update_party(self, d: Date):
+    for i in self.party:
+      i.update()
