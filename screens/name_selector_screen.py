a78c8f7856f1c7b62aafd07710d1216504643acd
Seamus Smith <student.141504@worcesterschools.net>
2020-11-03 18:12:31 +0000

    mostly completed wagon updating system

--- screens/name_selector_screen.py
+++ screens/name_selector_screen.py
@@ -14,7 +14,11 @@ class NameSelectorScreen(Screen):
     screen.fill(background)
     font = pygame.font.Font('font/font.ttf', 29)
     title = font.render('The Oregon Trail', True, WHITE)
-    self.render_multiline('What is the first name of the wagon\nleader? '+self.names[0], 25, 240, screen)
+    self.render_multiline_field('What is the first name of\nthe wagon leader? ', self.names[0], 25, 240, screen, self.selected == 0)
+    if self.selected > 0:
+      for i in range(4):
+        font = pygame.font.Font('font/font.ttf', 17)
+        self.render_multiline_field("#"+str(i+2)+": ",self.names[i+1], 25, 300+(i*30), screen, self.selected == i+1)
     screen.blit(title, (24, 40))
 
   def process_input(self, key):
@@ -32,8 +36,12 @@ class NameSelectorScreen(Screen):
       charKey = str(chr(key))
       self.names[self.selected] += charKey
     return self
-  def render_multiline(self, text, x, y, screen):
-    font = pygame.font.Font('font/font.ttf', 13)
+  def render_multiline_field(self, text, entered, x, y, screen, selected):
+    font = pygame.font.Font('font/font.ttf', 17)
+    if entered == "" and selected:
+      text += "_"
+    else:
+      text += entered
     lines = text.splitlines()
     for i, l in enumerate(lines):
-      screen.blit(font.render(l, 0, (255, 255, 255)), (x, y + 13 * i))
+      screen.blit(font.render(l, 0, (255, 255, 255)), (x, y + 17 * i))
--- wagon.py
+++ wagon.py
@@ -1,20 +1,40 @@
-from typing import Union
-from itembaseclass import BaseItem
-from itemstack import ItemStack
+from typing import Union, Any
 from date import Date
+from person import Person
+from enum import Enum
+
+# Wagon class
+# Author: Seamus Smith
 
 class Wagon:
-  def __init__(self, party: list):
+  class DamagedPart(Enum):
+    AXEL = 0
+    WHEEL = 1
+    TONGUE = 3
+  def __init__(self, party: list, main_character: Person):
     self.inventory: dict = {
       "food": 0,
       "cash": 0,
       "clothing": 0,
       "ammunition": 0,
       "axel": 0,
-      "wheel": 0
+      "wheel": 0,
+      "tongues": 0
     }
+    self.main_character = main_character
+    self.damaged: bool = False
+    self.damaged_part: Any
     self.oxen: list = []
     self.party: list = []
-  def update_party(self, d: Date):
+  #update all party members
+  def update_party(self) -> None:
     for i in self.party:
-      i.update()
+      i.update() if i.alive else (0)
+  # update oxen
+  def update_oxen(self) -> None:
+    for ox, i in enumerate(self.oxen):
+      ox.update() if ox.alive else self.oxen.remove(i)
+  def update_wagon(self) -> None:
+    raise NotImplementedError("the wagon crashed, burned, and blew up")
+    # should probably be a chance to damage the wagon, requiring spare parts
+
