77cb8d3bc4e781dd4c3ddefcf8a36b5a5260ef2e
Seamus Smith <student.141504@worcesterschools.net>
2020-11-05 18:23:25 +0000

    added imgscale_2d function

--- util.py
+++ util.py
@@ -1,9 +1,22 @@
 import pygame
+from math import *
+from typing import Union
 
-def imgscale(image, scale):
-  return pygame.transform.scale(image, (image.get_size()[0] * scale, image.get_size()[1] * scale))
+# Image Scaling function
+# By Seamus Smith
+# Copyright 2020-HeatDeathOfTheUniverse
+def imgscale(image: pygame.image , scale: Union[int, float]) -> pygame.image:
+  return pygame.transform.scale(image,
+  (round(image.get_size()[0] * scale), round(image.get_size()[1] * scale)))
 
-def is_letter(self, i):
+def imgscale_2d(image: pygame.image , scaleX: Union[int, float], scaleY: Union[int, float]) -> pygame.image:
+  return pygame.transform.scale(image,
+  (round(image.get_size()[0] * scaleX), round(image.get_size()[1] * scaleY)))
+
+# Is Letter Function
+# By Jack Richard
+# Copyright 2020
+def is_letter(i: chr) -> bool:
   try: 
     chr(i)
     return True
