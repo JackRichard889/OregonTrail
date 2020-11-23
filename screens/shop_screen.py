0ced5b36a1375750aabe02094e95a7bc06916f0a
Jack Richard <student.145024@worcesterschools.net>
2020-11-23 21:05:13 +0000

    Shop looks to be working, yay!

--- screens/shop_screen.py
+++ screens/shop_screen.py
@@ -3,14 +3,12 @@ from screens.alert_screen import AlertScreen
 import util
 import pygame
 
-# TODO: properly implement spare parts
-
 class ShopScreen(Screen):
   def __init__(self, data):
     self.data = data
     self.next = self
     self.idle = False
-    self.bill = {"oxen": 0, "food": 0, "clothing": 0, "ammunition": 0, "spare_parts": 0}
+    self.bill = {"oxen": 0, "food": 0, "clothing": 0, "ammunition": 0, "spare_parts": {"wheel": 0, "axel": 0,"tongue": 0}}
     self.money = data["wagon"].inventory["cash"]
     self.showScreen = 0
     self.input = [""]
@@ -35,7 +33,7 @@ class ShopScreen(Screen):
     elif self.showScreen == 2:
       space = font.render('Press SPACE to leave.', True, WHITE)
       screen.blit(space, (95, 475))
-      self.render_multiline('Shop: \n 1. Oxen \n 2. Food \n 3. Clothes\n 4. Ammo\n 5. Spare parts', 225, 20, screen)
+      self.render_multiline('Shop: \n 1. Oxen \n 2. Food \n 3. Clothes\n 4. Ammo\n 5. Spare parts', 205, 50, screen)
     elif self.showScreen == 3:
       self.render_multiline("Oxen:\n They're there to ride \n your wagon across \n the trail!", 50, 20, screen)
     elif self.showScreen == 4:
@@ -43,12 +41,14 @@ class ShopScreen(Screen):
     elif self.showScreen == 5:
       self.render_multiline("Clothes:\n You'll need them\n for the cruel winters!", 50, 20, screen)
     elif self.showScreen == 6:
-      self.render_multiline("Ammo:\n Use it if your in trouble \n or just need to hunt \n for some game.", 50, 20, screen)
-    elif self.showScreen == 7:
-      self.render_multiline("Spare parts:\n You'll need this in case your\n wagon breaks down!", 50, 20, screen)
+      self.render_multiline("Ammo:\n Use it if you're in\n trouble or just need to\n hunt for some game.", 50, 20, screen)
+    elif self.showScreen >= 7 and self.showScreen <= 9:
+      self.render_multiline("Spare parts:\n You'll need these in case your\n wagon breaks down!", 50, 20, screen)
+      options = ["wheels", "axels", "tongues"]
+      self.render_multiline_field("How many " + options[self.showScreen - 7] + " \nwould you like to\npurchase? ", self.input[0], 215, 150, screen, True)
     
     if self.showScreen > 2 and self.showScreen < 7:
-      options = ["many\nyokes (pairs)", "many pounds", "many\nsets", "many boxes of 20"]
+      options = ["many\nyokes (pairs)", "many pounds", "many\nsets", "many boxes\nof 20"]
       self.render_multiline_field("How " + options[self.showScreen - 3] + " would\nyou like to\npurchase? ", self.input[0], 225, 150, screen, True)
 
   def process_input(self, key):
@@ -61,7 +61,17 @@ class ShopScreen(Screen):
           self.bill[items[self.showScreen - 3]] = int(self.input[0])
           self.input[0] = ""
           self.showScreen = 2
-    elif key == pygame.K_BACKSPACE and self.showScreen < 7 and self.showScreen > 2 and len(self.input[0]) > 0:
+      elif self.showScreen >= 7 and self.showScreen <= 9:
+        items = ["wheel", "axel", "tongue"]
+        if int(self.input[0]) > 3:
+          self.next = AlertScreen(self.data, "You can only hold\n3 of these.")
+        else:
+          self.bill["spare_parts"][items[self.showScreen - 7]] = int(self.input[0])
+          self.input[0] = ""
+          self.showScreen += 1
+          if self.showScreen > 9:
+            self.showScreen = 2
+    elif key == pygame.K_BACKSPACE and self.showScreen < 10 and self.showScreen > 2 and len(self.input[0]) > 0:
       self.input[0] = self.input[0][:-1]
     elif util.is_letter(key):
       charKey = str(chr(key))
@@ -69,9 +79,11 @@ class ShopScreen(Screen):
         if self.showScreen < 2:
           self.showScreen += 1
         elif self.showScreen == 2:
-          # TODO: check minimum purchases (e.g you need oxen to start the game)
-          print("Leaving store with "+str(self.bill))
-      elif self.showScreen > 2 and self.showScreen < 7 and self.isNumber(charKey):
+          if self.bill["oxen"] > 0:
+            print("Leaving store with: "+str(self.bill))
+          else:
+            self.next = AlertScreen(self.data, "You need some oxen to\npull your wagon!")
+      elif self.showScreen > 2 and self.showScreen < 10 and self.isNumber(charKey):
         self.input[0] += charKey
       elif self.showScreen == 2 and self.isNumber(charKey):
         self.showScreen = int(charKey) + 2
